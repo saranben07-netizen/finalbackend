ead6f9c07f5792764472a863b586b043fc998465
mega9789728960 <benmega500@gmail.com>
2025-09-15 18:39:19 +0530

    frf

--- controllers/authorisation.js
+++ controllers/authorisation.js
@@ -5,7 +5,7 @@ async function authorisation(req, res, next) {
   // 1️⃣ Get tokens
   const token = req.body.token || req.headers["authorization"]?.split(" ")[1];
   const refreshToken = req.cookies.refreshToken;
-  console.log(refreshToken);
+  console.log("cookies",refreshToken);
 
   if (!token) {
     return res.status(401).json({ success: false, error: "Token missing" });
