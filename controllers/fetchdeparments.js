ff47974becf205b3ef3dfc228006f61aaa02fad5
mega9789728960 <benmega500@gmail.com>
2025-09-25 12:40:53 +0530

    bg

--- controllers/fetchdeparments.js
+++ controllers/fetchdeparments.js
@@ -3,12 +3,12 @@ import pool from "../database/database.js";
 
 async function fetchdepartments(req, res) {
   try {
-    const result = await pool.query(`SELECT department FROM departments`);
+    const result = await pool.query(`SELECT * FROM departments`);
     
     // ✅ Convert rows into list of strings
     const departments = result.rows.map(row => row.department);
 
-    return res.json({ success: true, result: result.rows[0] });
+    return res.json({ success: true, result: result.rows });
   } catch (error) {
     console.error("Error fetching departments:", error);
     return res.status(500).json({ success: false, message: "Internal server error"});
