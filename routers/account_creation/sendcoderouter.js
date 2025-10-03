84e7618a1b841d39b4e7a59f52c3dd341b02bcd0
mega9789728960 <benmega500@gmail.com>
2025-10-03 18:17:30 +0530

    fix

--- routers/account_creation/sendcoderouter.js
+++ routers/account_creation/sendcoderouter.js
@@ -1,27 +1,11 @@
 import express from "express";
 import sendcode from "../../controllers/account_creation/sendcode.js";
-import { RateLimiterMemory } from "rate-limiter-flexible";
+
 
 const sendcoderouter = express.Router();
 
 // ✅ 5 requests per 10 minutes per IP
-const sendCodeLimiter = new RateLimiterMemory({
-  points: 5,
-  duration: 600, // 600 seconds = 10 mins
-});
 
-async function rateLimiterMiddleware(req, res, next) {
-  try {
-    await sendCodeLimiter.consume(req.ip);
-    next();
-  } catch {
-    res.status(429).json({
-      success: false,
-      message: "Too many attempts. Please wait 10 minutes.",
-    });
-  }
-}
-
-sendcoderouter.post("/sendcode", rateLimiterMiddleware, sendcode);
+sendcoderouter.post("/sendcode",  sendcode);
 
 export default sendcoderouter;
