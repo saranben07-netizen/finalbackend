acf80d92a3180df2d360971ecf46afb3beea9290
mega9789728960 <benmega500@gmail.com>
2025-10-10 17:36:36 +0530

    efr

--- controllers/admin/attendance/changeattendanceforadmin.js
+++ controllers/admin/attendance/changeattendanceforadmin.js
@@ -4,7 +4,7 @@ async function changeattendancebyadmin(req, res) {
   try {
     const { attendance_id, update ,token,...data} = req.body;
     // normalize status
-const status = req.body.update.trim().toLowerCase(); // or .toUpperCase() depending on your DB constraint
+ // or .toUpperCase() depending on your DB constraint
 
 
     // ✅ Validate input
@@ -23,13 +23,13 @@ const status = req.body.update.trim().toLowerCase(); // or .toUpperCase() depend
       ON CONFLICT (student_id, date) DO NOTHING
       RETURNING *;
     `;
-    const result = pool.query(query,[student_id,status])
+    const result = pool.query(query,[student_id,update])
     return res.json({success:true,token})
     }
 
     // ✅ Run update query
     const query = "UPDATE attendance SET status = $1 WHERE id = $2";
-    const result = await pool.query(query, [status, attendance_id]);
+    const result = await pool.query(query, [update, attendance_id]);
 
 
     // ✅ Check if record exists
